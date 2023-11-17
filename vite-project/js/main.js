edd2792655a7ebf42a4f3a5e2da377f33d0dfc9d
ashleyyz068 <144456040+ashleyyz068@users.noreply.github.com>
2023-11-17 12:25:45 -0500

    d

--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -8,10 +8,6 @@
 import "../styles/style.css"
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
-// ..
 AOS.init();
 import { menu } from "./menu"; 
-menu. forEach((drink)=>{
-    
-}
-)
+import { DOMselectors } from "./Dom";
